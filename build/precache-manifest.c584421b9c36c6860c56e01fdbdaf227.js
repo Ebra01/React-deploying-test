d649261a1a5f197290dc2c619b6e41cf729d5035
Ebrahim AlBlooshi <ebrahim53033@gmail.com>
2020-04-12 03:14:39 +0400

    React-App

---
+++ build/precache-manifest.c584421b9c36c6860c56e01fdbdaf227.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e201a07a2bfcfb958fb653c086f7c0f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6035ebd0da8a25b3a6c7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3bce2679420501223c01",
+    "url": "/static/js/2.2b26d941.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b26d941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6035ebd0da8a25b3a6c7",
+    "url": "/static/js/main.4a351de3.chunk.js"
+  },
+  {
+    "revision": "f8d5b9075381204bfc76",
+    "url": "/static/js/runtime-main.39e8876a.js"
+  }
+]);
